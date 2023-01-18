8b5843cbfd123cffe26572f248646348d897e4b4
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2023-01-18 16:20:19 +0100

    refaktoryzacja go brrr

--- server.py
+++ server.py
@@ -9,9 +9,6 @@ class Database:
     def __init__(self):
         self.conn = sqlite3.connect('chat.db')
         self.cursor = self.conn.cursor()
-
-
-
         self.cursor.execute('''
         CREATE TABLE IF NOT EXISTS messages (
             id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -21,7 +18,6 @@ class Database:
         )
         ''')
 
-
     def add_message(self, server, username, message):
         self.cursor.execute('''
             INSERT INTO messages (server, username, message)
@@ -30,7 +26,6 @@ class Database:
         self.conn.commit()
 
     def get_last_messages(self, server, count=25):
-        # Fetch the last count messages from the messages table
         self.cursor.execute('''
             SELECT *
             FROM messages
@@ -55,41 +50,50 @@ db = Database()
 
 @app.get("/{server}/{user}")
 async def get_index(request: Request, server: str, user: str):
-    host = str(request.base_url).split("//")[1].split("/")[0]
-    print(host)
-    template = open("./index.html", "r").read()
+    base_url = str(request.base_url)
+    stripped_protocol = base_url.split("//")[1]
+    host = stripped_protocol.split("/")[0]
     
-    return HTMLResponse(template.replace("{{host}}", host).replace("{{server}}", server).replace("{{user}}", user))
+    template = open("./index.html", "r").read()
+    rendered_template = template.replace("{{host}}", host) \
+        .replace("{{server}}", server) \
+        .replace("{{user}}", user)
+    
+    return HTMLResponse(rendered_template)
 
 servers: dict[bytes, list[WebSocket]] = {}
+
+async def send_message(to: WebSocket, user: str, message: str, end: bool = True):
+    await to.send_json({
+        "user": user, 
+        "message": message, 
+        "end": end
+    })
 
 @app.websocket("/chat")
 async def chat(websocket: WebSocket):
     await websocket.accept()
 
     server, user = websocket.headers.get("sec-websocket-protocol").split("|")
-    if server is None:
-        server = "default"
 
     if server not in servers:
         servers[server] = []
     servers[server].append(websocket)
 
     last = db.get_last_messages(server)
-    print(last)
     for item in last:
-        await websocket.send_json({"user": item[2], "message": item[3], "end": True})
-
+        await send_message(websocket, *item[2:])
+        
     try:
         while True:
             data = await websocket.receive_json()
             if data["type"] == "message":
                 db.add_message(server, user, data["message"])
                 for client in servers[server]:
-                    await client.send_json({"user": user, "message": data["message"], "end": True})
+                    await send_message(client, user, data["message"])
             else:
                 for message in db.get_messages_before(server, data["message_id"]):
-                    await websocket.send_json({"user": message[2], "message": message[3], "end": False})
+                    await send_message(websocket, *message[2:], False)
     except WebSocketDisconnect:
         servers[server].remove(websocket)
         await websocket.close()
